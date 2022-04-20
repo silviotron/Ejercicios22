80bd72c3cba7e4e6673dea433915711a46b85403
silviotron <silvionovasmartinezgmail.com>
2022-04-20 10:54:15 +0200

    + first

---
+++ src/ejercicios22/clases/Pieza.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicios22.clases;
+
+import com.google.common.base.Preconditions;
+
+/**
+ *
+ * @author alumno
+ */
+public abstract class Pieza{
+    private Posicion posicion;
+    private final boolean isBlanco;
+
+    public Pieza(Posicion posicion, boolean isBlanco) {
+        this.posicion = posicion;
+        this.isBlanco = isBlanco;
+    }
+
+    public void setPosicion(Posicion p) {
+        Preconditions.checkNotNull(p);
+        this.posicion = p;
+    }
+
+    public Posicion getPosicion() {
+        return posicion;
+    }
+    
+    public boolean mover(Posicion p){
+        Preconditions.checkNotNull(p);
+        if(this.posicion == null){
+            return false;
+        }
+        if(this.posicion.equals(p)){
+            return false;
+        }
+        if(!isPosicionDentro(p)){
+            return false;
+        }
+        if (checkPieza(p)) {
+            this.posicion = p;
+            return true;
+        }
+        return false;
+        
+        
+    }
+
+    abstract public boolean checkPieza(Posicion p);   
+    
+    private boolean isPosicionDentro(Posicion p){
+        return p.getX() >= 1 || p.getX() <= 8 && p.getY() >= 1 || p.getY() <= 8;
+        
+    }
+    
+    private static void checkPosicion(Number n) {
+        Preconditions.checkArgument(n.doubleValue() >= 1 || n.doubleValue() <= 8);
+    }     
+    
+    
+}
---
+++ src/ejercicios22/clases/Posicion.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicios22.clases;
+
+import com.google.common.base.Preconditions;
+import java.util.Objects;
+
+/**
+ *
+ * @author alumno
+ */
+public class Posicion implements Comparable<Posicion>{
+    private int x;
+    private int y;
+
+    public Posicion(int x, int y) {
+//        check1y8(x);
+//        check1y8(y);
+        this.x = x;
+        this.y = y;
+    }     
+    
+    private static void check1y8(Number n) {
+        Preconditions.checkArgument(n.doubleValue() >= 1 || n.doubleValue() <= 8);
+    }     
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(this.toString());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        } else if (obj == this) {
+            return true;
+        } else if (obj instanceof Posicion) {
+            Posicion aux = (Posicion) obj;
+            return this.x == aux.x && this.y == aux.y;
+        } else {
+            return false;
+        }
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public int compareTo(Posicion p) {
+        Preconditions.checkNotNull(p);
+        if(Integer.compare(this.x, p.x) != 0){
+            return Integer.compare(this.x, p.x);
+        }else{
+            return Integer.compare(this.y, p.y);
+        }
+        
+    }
+    
+    
+    
+}
